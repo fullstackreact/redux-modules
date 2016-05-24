c40447c98c98ee8a0aa29edfc84e51845ac2e652
Ari Lerner <arilerner@mac.com>
2016-05-24 14:16:05 -0700

    export createReducer from index

--- src/index.js
+++ src/index.js
@@ -1,31 +1,17 @@
-import invariant from 'invariant'
-
-import {
-    createConstants
-} from './lib/constants';
-
-import { apiClient } from './lib/apiClient.js'
-
-import {
-  createApiMiddleware
-} from './lib/createApiMiddleware'
-
-import {
-  createApiAction,
-  api
-} from './lib/createApiActions';
-
-import {
-  createApiHandler,
-  apiHandler
-} from './lib/createApiHandler';
+import { createConstants } from './lib/constants';
+import { createReducer } from './lib/createReducer';
+import { apiClient } from './lib/apiClient.js';
+import { createApiMiddleware } from './lib/createApiMiddleware';
+import { createApiAction, api } from './lib/createApiActions';
+import { createApiHandler, apiHandler } from './lib/createApiHandler';
 
 export default {
-    createConstants,
-    apiClient,
-    createApiMiddleware,
-    createApiAction,
-    api,
-    createApiHandler,
-    apiHandler
+  createConstants,
+  createReducer,
+  apiClient,
+  createApiMiddleware,
+  createApiAction,
+  api,
+  createApiHandler,
+  apiHandler
 }
