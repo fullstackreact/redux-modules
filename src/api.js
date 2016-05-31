338d7b02cb4b05a33356e7be39703db194b421c8
Ari Lerner <arilerner@mac.com>
2016-05-31 01:03:48 -0700

    Separated api from index

---
+++ src/api.js
@@ -0,0 +1,4 @@
+export { apiClient } from './apiClient.js';
+export { createApiMiddleware } from './createApiMiddleware';
+export { createApiAction, api } from './createApiActions';
+export { createApiHandler, apiHandler } from './createApiHandler';
--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 export { createConstants } from './lib/constants';
 export { createReducer } from './lib/createReducer';
-export { apiClient } from './lib/apiClient.js';
-export { createApiMiddleware } from './lib/createApiMiddleware';
-export { createApiAction, api } from './lib/createApiActions';
-export { createApiHandler, apiHandler } from './lib/createApiHandler';
 export { bindActionCreatorsToStore } from './lib/bindActionCreatorsToStore';
