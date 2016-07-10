bda81eaeaf361a485f1f72fef64bdc36cba0c5e7
Ari Lerner <arilerner@mac.com>
2016-07-10 01:52:41 -0700

    export createRootReducer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { createConstants } from './lib/constants';
 export { createReducer } from './lib/createReducer';
 export { bindActionCreatorsToStore } from './lib/bindActionCreatorsToStore';
+export { createRootReducer } from './lib/createRootReducer';
